db5e1d6fe021dd1ef523c84ac8a9da6a62f44dac
Steve Jones <jonesst2608@gmail.com>
2024-01-13 09:56:27 +0000

    Tidy up

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -4,6 +4,10 @@ from reactpy.testing import poll
 
 from reactpy_select.counter import ExampleCounter
 
+# Basic test of the default custom component example.
+#
+#  https://github.com/reactive-python/reactpy-js-component-template/blob/main/%7B%7Bcookiecutter.repository_name%7D%7D/tests/test_example.py
+
 @pytest.mark.anyio
 async def test_example_counter(display):
     count = reactpy.Ref(0)
