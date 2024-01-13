075f9a4dee5ba141fed5062c1c993704a7e93469
Steve Jones <jonesst2608@gmail.com>
2024-01-13 10:02:10 +0000

    Added more tests

---
+++ tests/examples/test_simple.py
@@ -0,0 +1,35 @@
+import pytest
+from reactpy.testing import DisplayFixture
+from examples.simple import AppMain
+
+from ..tooling.wait_stable import wait_page_stable
+
+options = [
+  { 'value': 'chocolate', 'label': 'Chocolate' },
+  { 'value': 'strawberry', 'label': 'Strawberry' },
+  { 'value': 'vanilla', 'label': 'Vanilla' }
+]
+
+# https://playwright.dev/python/docs/next/locators
+
+@pytest.mark.anyio
+async def test_example_simple(display: DisplayFixture):
+    await display.show(AppMain)
+    await wait_page_stable(display.page)
+
+    # Unselected state
+
+    text = await display.page.locator('id=dd1').all_inner_texts()
+    assert text == ['Select...']
+
+    # Dropdown state, verify all options are listed in the dropdown
+
+    await display.page.locator('id=dd1').click()
+    text = await display.page.locator('role=listbox').all_inner_texts()
+    assert text == ['Chocolate\nStrawberry\nVanilla']
+
+    # Select an option from the dropdown and confirm the selection
+
+    await display.page.locator('text=Strawberry').click()
+    text = await display.page.locator('id=dd1').all_inner_texts()
+    assert text == ['option Strawberry, selected.\nStrawberry']
