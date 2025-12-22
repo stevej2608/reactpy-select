2f8806c5963d3fb8d3b8fbfd54bc29689edd06f6
Steve Jones <jonesst2608@gmail.com>
2025-12-22 11:42:00 +0000

    Tidy up

--- __main__.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-if not __package__:
-    # Make CLI runnable from source tree with
-    #    python src/package
-    package_source_path = os.path.dirname(os.path.dirname(__file__))
-    sys.path.insert(0, package_source_path)
